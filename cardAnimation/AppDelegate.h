25b87ce4559d366527ea4d7326cecf2d8ac8324f
jft0m <377632523@qq.com>
2018-12-05 17:29:19 +0800

    init `

---
+++ cardAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cardAnimation
+//
+//  Created by jft0m on 2018/12/5.
+//  Copyright © 2018 jft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cardAnimation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  cardAnimation
+//
+//  Created by jft0m on 2018/12/5.
+//  Copyright © 2018 jft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
